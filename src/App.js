83027c2295f84c22af2bb5d312eae03126b55fc2
at51401ansari-Adil-taimoor <at51401ansari@gmail.com>
2026-05-01 17:13:23 -0700

    Module 3 frontend screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchResults from "./components/SearchResults";
+import CategoryPage from "./components/CategoryPage";
+import ProjectStatus from "./components/ProjectStatus";
+import ProjectDetail from "./components/ProjectDetail";
+import Notifications from "./components/Notifications";
+import ClientDashboard from "./components/ClientDashboard";
+import PostAJob from "./components/PostAJob";
+import MyJobs from "./components/MyJobs";
+import JobDetail from "./components/JobDetail";
+import EditJob from "./components/EditJob";
+import BidsForJob from "./components/BidsForJob";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchResults />} />
+        <Route path="/category" element={<CategoryPage />} />
+        <Route path="/project-status" element={<ProjectStatus />} />
+        <Route path="/project-detail" element={<ProjectDetail />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/client-dashboard" element={<ClientDashboard />} />
+        <Route path="/post-a-job" element={<PostAJob />} />
+        <Route path="/my-jobs" element={<MyJobs />} />
+        <Route path="/job-detail" element={<JobDetail />} />
+        <Route path="/edit-job" element={<EditJob />} />
+        <Route path="/bids" element={<BidsForJob />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
